f18d65030cf60687fafe6142758b9c420ab5c3f2
lucasbezerrademacedo <lucasbezerrademacedo@192.168.0.17>
2025-02-24 13:37:57 -0300

    Segundo commit.

--- src/controller/QtdDeNumeros.java
+++ src/controller/QtdDeNumeros.java
@@ -7,10 +7,13 @@ public class QtdDeNumeros {
 	}
 	public int QtdResultado (int numero) {
 		if (numero < 10) {
-			return 1;
+			return 1; /* Condicao de parada: Quando o numero fornecido for menor que 10 ele vai retornar 1 como digito. 
+			Poderá somar também esse 1 com os retornos do else. */
 		}
 		else {
-			return 1 + QtdResultado(numero/10);			
+			return 1 + QtdResultado(numero/10); /* Se o numero for maior que 10, haverá 
+			o retorno de 1 + o numero dividido por 10 e ira fazer isso até o numero ser menor que 10. 
+			No final, irá somar 1 em todos os retornos e no finbal retornará apenas a parte inteira. */
 		}
 	}
 }
